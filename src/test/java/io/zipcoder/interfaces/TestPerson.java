f31981be5edb42567582829fb20663dd93c78c45
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-07 09:33:16 -0500

    first commit

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,29 @@
 package io.zipcoder.interfaces;
 
-public class TestPerson {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestPerson {
+    @Test
+    public void PersonConstructorTest() {
+        Person person = new Person(444L, "Brian");
+        Long expectedId = 444L;
+        Long actualId = person.getId();
+        String expectedName = "Brian";
+        String actualName = person.getName();
+        Assert.assertEquals(expectedId,actualId);
+        Assert.assertEquals(expectedName,actualName);
+
+    }
+
+
+    @Test
+    public void setNameTest(){
+        Person personOne = new Person(444L, "Brian");
+        personOne.setName("Jack");
+        String expectedName = "Jack";
+        String actualName = personOne.getName();
+
+        Assert.assertEquals(expectedName,actualName);
+    }
 }
